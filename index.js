09b2076a965478d63dcdd54b7a4afce6849d5405
Big mike <mikendirangu26@gmail.com>
2025-06-14 11:24:08 +0300

    welcomeMessage

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+//const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
